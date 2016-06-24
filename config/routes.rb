81b4d5169bd5d6d8c5f17a3e6933bab30c7d933a
Carlos <c.salas.marin@gmail.com>
2016-06-23 22:13:48 -0400

    avanzooo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :teachers
+
   devise_for :users
+
   get 'teachers/index'
 
   get 'teachers/show'
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'teachers/stats'
 
+
+   resources :teachers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
